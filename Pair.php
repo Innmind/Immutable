d47a6ea746430f6cfbdb3faffe1afd80e6d56403
Baptiste Langlade <baptouuuu@gmail.com>
2016-02-28 16:53:23 +0100

    remove the need for a pair key to be a symbol

--- Pair.php
+++ Pair.php
@@ -11,13 +11,13 @@ class Pair
     private $key;
     private $value;
 
-    public function __construct(Symbol $key, $value)
+    public function __construct($key, $value)
     {
         $this->key = $key;
         $this->value = $value;
     }
 
-    public function key(): Symbol
+    public function key()
     {
         return $this->key;
     }
