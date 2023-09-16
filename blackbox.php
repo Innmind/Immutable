3532ca59100187d7387b74dda097c9fe0ed7def1
Baptiste Langlade <baptouuuu@gmail.com>
2023-09-16 14:31:20 +0200

    simplify blackbox config

--- blackbox.php
+++ blackbox.php
@@ -10,18 +10,19 @@ use Innmind\BlackBox\{
 };
 
 Application::new($argv)
-    ->codeCoverage(
-        CodeCoverage::of(
-            __DIR__.'/src/',
-            __DIR__.'/proofs/',
-            __DIR__.'/fixtures/',
-        )
-            ->dumpTo('coverage.clover')
-            ->enableWhen(\getenv('ENABLE_COVERAGE') !== false),
+    ->when(
+        \getenv('ENABLE_COVERAGE') !== false,
+        static fn(Application $app) => $app
+            ->codeCoverage(
+                CodeCoverage::of(
+                    __DIR__.'/src/',
+                    __DIR__.'/proofs/',
+                    __DIR__.'/fixtures/',
+                )
+                    ->dumpTo('coverage.clover')
+                    ->enableWhen(true),
+            )
+            ->scenariiPerProof(1),
     )
-    ->scenariiPerProof(match (\getenv('ENABLE_COVERAGE')) {
-        false => 100,
-        default => 1,
-    })
     ->tryToProve(Load::everythingIn(__DIR__.'/proofs/'))
     ->exit();
