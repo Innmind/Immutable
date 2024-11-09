f3cd65a7fa901b86dc65933a821b8fce04fc5b7f
Baptiste Langlade <baptouuuu@gmail.com>
2024-11-09 17:34:44 +0100

    Revert "increase the number of scenarii per proof in the CI"
    This reverts commit 2ed448212395ac5ba9252406f7522a02a40ce659.

--- blackbox.php
+++ blackbox.php
@@ -24,9 +24,5 @@ Application::new($argv)
             )
             ->scenariiPerProof(1),
     )
-    ->when(
-        \getenv('CI') !== false,
-        static fn(Application $app) => $app->scenariiPerProof(1_000),
-    )
     ->tryToProve(Load::everythingIn(__DIR__.'/proofs/'))
     ->exit();
